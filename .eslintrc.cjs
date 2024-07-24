94bbd65e95fe17f54d0c85ba30df4793f154baa2
ndrxwsh <andreyshihalev099@gmail.com>
2024-07-24 13:57:55 +0700

    chore: update styling rules and deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,66 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx', '.tsx']
+      }
+    ],
+    'react/display-name': 'off',
+    'import/no-unresolved': 'off',
+    'no-param-reassign': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'no-undef': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    'comma-dangle': ['error', 'never'],
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/jsx-curly-newline': ['error', 'consistent'],
+    'react/jsx-wrap-multilines': [
+      'error',
+      {
+        declaration: false,
+        assignment: false,
+        return: true
+      }
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreComments: true
+      }
+    ],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off'
+  }
 }
