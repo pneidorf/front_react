d41cc41d14880d7bfae8e5dd816004390d5e8150
ndrxwsh <andreyshihalev099@gmail.com>
2024-08-09 14:09:47 +0700

    feat: add dark theme for map

--- src/entities/map/ui/map.tsx
+++ src/entities/map/ui/map.tsx
@@ -1,5 +1,6 @@
 import { Map as MapLibreMap, NavigationControl } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import { useTheme } from 'next-themes'
 import { FC, useEffect, useState } from 'react'
 
 export const Map: FC = () => {
@@ -8,6 +9,9 @@ export const Map: FC = () => {
   const [lat] = useState(55.0152)
   const [zoom] = useState(13)
 
+  const { theme } = useTheme()
+  const mapStyle = theme === 'dark' ? 'streets-dark' : 'streets'
+
   useEffect(() => {
     if (!mapReady) return
 
@@ -15,14 +19,14 @@ export const Map: FC = () => {
       container: 'central-map',
       center: [lng, lat],
       zoom: zoom,
-      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${import.meta.env.VITE_MAPLIBRE_API_KEY}`
+      style: `https://api.maptiler.com/maps/${mapStyle}/style.json?key=${import.meta.env.VITE_MAPLIBRE_API_KEY}`
     })
 
     const nav = new NavigationControl({
       visualizePitch: true
     })
     map.addControl(nav, 'top-left')
-  }, [lat, lng, mapReady, zoom])
+  }, [lat, lng, mapReady, mapStyle, zoom])
 
   return <div className='h-full w-full' ref={() => setMapReady(true)} id='central-map' />
 }
