35a88f102089261f519037a70a31c80b861d4569
ndrxwsh <andreyshihalev099@gmail.com>
2024-08-12 21:41:27 +0700

    fix: date issue

--- src/shared/ui/date.tsx
+++ src/shared/ui/date.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 function formatDate(date: Date) {
   const optionsDate: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long', year: 'numeric' }
   const optionsTime: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit' }
@@ -9,6 +11,14 @@ function formatDate(date: Date) {
 }
 
 export const CurrentDate = () => {
-  const date = new Date()
+  const [date, setData] = useState<Date>(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setData(new Date())
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   return <p className='text-lg font-bold text-primary'>{formatDate(date)}</p>
 }
