d1433050c9d6a29e77b25fc8a3f9a99b64b7f088
ndrxwsh <andreyshihalev099@gmail.com>
2024-08-12 16:27:18 +0700

    refactor: map container

--- src/entities/map/ui/map.tsx
+++ src/entities/map/ui/map.tsx
@@ -1,22 +1,24 @@
 import { Map as MapLibreMap, NavigationControl } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import { useTheme } from 'next-themes'
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 
 export const Map: FC = () => {
-  const [mapReady, setMapReady] = useState(false)
   const [lng] = useState(82.9296)
   const [lat] = useState(55.0152)
   const [zoom] = useState(13)
+
+  const map = useRef<MapLibreMap | null>(null)
+  const mapContainer = useRef(null)
 
   const { theme } = useTheme()
   const mapStyle = theme === 'dark' ? 'streets-dark' : 'streets'
 
   useEffect(() => {
-    if (!mapReady) return
+    if (!mapContainer.current) return
 
-    const map = new MapLibreMap({
-      container: 'central-map',
+    map.current = new MapLibreMap({
+      container: mapContainer.current,
       center: [lng, lat],
       zoom: zoom,
       style: `https://api.maptiler.com/maps/${mapStyle}/style.json?key=${import.meta.env.VITE_MAPLIBRE_API_KEY}`
@@ -25,8 +27,8 @@ export const Map: FC = () => {
     const nav = new NavigationControl({
       visualizePitch: true
     })
-    map.addControl(nav, 'top-left')
-  }, [lat, lng, mapReady, mapStyle, zoom])
+    map.current.addControl(nav, 'top-left')
+  }, [lat, lng, mapStyle, zoom])
 
-  return <div className='h-full w-full' ref={() => setMapReady(true)} id='central-map' />
+  return <div className='h-full w-full' ref={mapContainer} />
 }
